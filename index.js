b27c3f51ee8d18798b1ed57946bfe500c1987f3c
Austin Reuland <amreuland@gmail.com>
2016-06-07 20:01:34 -0500

    Almost done

--- index.js
+++ index.js
@@ -2,6 +2,11 @@
 
 const Ashe = require('./lib/client')
 const constants = require('./lib/constants')
-const
+
+Ashe.Maps = constants.maps
+Ashe.GameModes = constants.gameModes
+Ashe.GameTypes = constants.gameTypes
+Ashe.Regions = constants.regions
+Ashe.QueueTypes = constants.queueTypes
 
 module.exports = Ashe
